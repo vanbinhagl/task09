1c2765dbabf7799306fbfa04b68338934b0fce75
vanbinhagl <vanbinh.agl@gmail.com>
2023-03-10 17:16:18 +0700

    init file

---
+++ src/_public/assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.c-mainvisual__slider').slick({
+        fade: true,
+        speed: 1100,
+        infinite: true,
+        autoplay: true,
+        cssEase: "linear",
+        arrows: false,
+    });
+  });
